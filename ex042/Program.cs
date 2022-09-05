a23dc40a185de8dd6a0dfc65af72748ac8df2548
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-06 01:30:24 +0600

    перевод числа из десятичной системы в двоичную

---
+++ ex042/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int numb = int.Parse(Console.ReadLine());
+int a = 0;
+
+string b = "";
+
+while (numb >= 1)
+{
+    a = numb % 2;
+    b = a + b;
+
+    numb = numb / 2;
+}
+Console.WriteLine(b);
