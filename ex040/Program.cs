28d026d429f92289ab08beced64859db958e5454
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-06 01:28:19 +0600

    теорема о неравенстве треугольника

---
+++ ex040/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+
+if (a + b > c && b + c > a && a + c > b)
+{
+    Console.WriteLine(" такой треугольник существует!");
+}
+else
+    Console.WriteLine(" такой треугольник не существует!");
