1275cfa8f92fd9c32f223066d28086b41d7d0f01
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-06 01:27:01 +0600

    перепись массива задом наперед

---
+++ ex/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] GetArray(int size, int minV, int maxV)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; ++i)
+    {
+        res[i] = new Random().Next(minV, maxV + 1);
+    }
+    return res;
+}
+
+int[] arr = GetArray(10, 0, 10);
+Console.WriteLine(string.Join(",", arr));
+for (int i = 0; i < arr.Length / 2; ++i)
+{
+    int temp = arr[i];
+    arr[i] = arr[arr.Length - i - 1];
+    arr[arr.Length - i - 1] = temp;
+}
+Console.WriteLine(string.Join(",", arr));
