04eaf9a0b436715d096d2b0200f29b5f2c0bba1c
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-06 01:32:09 +0600

    вывод N элементов из последовательнности чисел фибоначи

---
+++ ex044/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+//int[] array1 = {0,1}; // i
+int[] array2 = new int[a]; //j
+
+array2[0] = 0;
+array2[1] = 1;
+
+for (int j = 2; j < a; j++)
+{
+    array2[j] = array2[j - 1] + array2[j - 2];
+}
+Console.WriteLine(string.Join(" ", array2));
