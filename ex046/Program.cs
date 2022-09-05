4242f1bb355ac964e24a83511413c130d59bad78
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-06 01:33:55 +0600

    поэлементное копирование одного массива в другой

---
+++ ex046/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] GetArray(int size, int minV, int maxV)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; ++i)
+    {
+        res[i] = new Random().Next(minV, maxV + 1);
+    }
+    return res;
+}
+
+int[] arr = GetArray(10, 0, 10); // i
+Console.WriteLine(string.Join(",", arr));
+
+int[] arr2 = new int[arr.Length];
+
+for (int i =0; i < arr.Length; i++)
+{
+    arr2[i] = arr[i];
+}
+//Console.WriteLine(string.Join(",", arr));
+Console.WriteLine(string.Join(",", arr2));
