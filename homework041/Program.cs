eb8f772178b442301bc3b2801cb03a19ab2da106
Tatyana <Bagira-mur-rr@mail.ru>
2022-09-08 02:21:37 +0600

    количество чисел больше нуля из введенных с клавиатуры

---
+++ homework041/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите количество чисел, которое хотите ввести: ");
+int m = int.Parse(Console.ReadLine());
+int[] arr = new int[m];
+
+int count = 0;
+
+for (int j = 0; j < m; j++)
+{
+    Console.Write("Введите любое число: ");
+    int a = int.Parse(Console.ReadLine());
+
+    arr[j] = a;
+
+    if (arr[j] > 0)
+    {
+        count += 1;
+    }
+}
+//Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine();
+Console.WriteLine("Количество чисел больше нуля -> " + count);
